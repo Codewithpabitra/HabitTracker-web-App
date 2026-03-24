c4d4e54c65f88ce869bbccdb9647e53b7af53a0a
Codewithpabitra <pabitra05.maity@gmail.com>
2026-03-25 00:02:48 +0530

    update vercel json'

--- server/server.js
+++ server/server.js
@@ -8,11 +8,12 @@ import serverless from "serverless-http";
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 const handler = serverless(app);
 
