139464f53712022a8bbccd4d8eb19831ed2cdfcf
Codewithpabitra <pabitra05.maity@gmail.com>
2026-03-25 01:52:47 +0530

    db update

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,42 +1,21 @@
-import dotenv from "dotenv";
-dotenv.config();
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
-
-if (!MONGO_URI) {
-  throw new Error("Please define MONGO_URI");
-}
-
-// 👇 GLOBAL cache (Vercel best practice)
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+let isConnected = false; // cache
 
 const connectDB = async () => {
-  if (cached.conn) {
-    console.log("Using cached DB connection");
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
-    }).then((mongoose) => mongoose);
+  if (isConnected) {
+    console.log("Using existing DB connection");
+    return;
   }
 
   try {
-    cached.conn = await cached.promise;
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    isConnected = db.connections[0].readyState === 1;
+
     console.log("MongoDB connected");
   } catch (error) {
-    cached.promise = null;
-    console.error("DB Error:", error.message);
-    throw error;
+    console.error("Error in DB Connection", error);
   }
-
-  return cached.conn;
 };
 
 export default connectDB;
