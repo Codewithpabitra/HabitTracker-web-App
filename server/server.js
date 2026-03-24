f86f2fca66d1d6d99dc7bc10e76578b5078b6b3d
Codewithpabitra <pabitra05.maity@gmail.com>
2026-03-24 23:42:18 +0530

    add vercel json file

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import app from "./src/app.js"
 import connectDB from "./src/config/db.js"
+import serverless from "serverless-http";
 
 
 connectDB();
@@ -12,3 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
 });
+
+const handler = serverless(app);
+
+export default handler;
