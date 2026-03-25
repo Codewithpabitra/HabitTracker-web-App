31a6ddb069f2d0bb910c585cb8a704cc65b6991d
Codewithpabitra <pabitra05.maity@gmail.com>
2026-03-25 18:48:30 +0530

    add cors

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,9 @@ import errorHandler from "./middlewares/error.middleware.js"
 const app = express()
 
 // middlewares
-app.use(cors())
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json())
 app.use(morgan("dev"))
 
